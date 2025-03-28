1b2711749e0eb04cf1c09524e95380170f49ff94
Charlie <hansmadethis@gmail.com>
2025-03-28 11:01:03 -0400

    Update standup.py

--- standup.py
+++ standup.py
@@ -7,7 +7,6 @@ def connect_jira(log, jira_server, jira_user, api_token):
         log.info("Connecting to JIRA: %s" % jira_server)
         jira_options = {'server': jira_server}
         jira = JIRA(options=jira_options, basic_auth=(jira_user, api_token))
-                                        # ^--- Note the tuple
         return jira
     except Exception(e):
         log.error("Failed to connect to JIRA: %s" % e)
