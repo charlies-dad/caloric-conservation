cab89ca0375d79c1bbb21dbb3d834b2b9455971a
Charlie <hansmadethis@gmail.com>
2025-03-28 14:50:30 -0400

    Update standup.py

--- standup.py
+++ standup.py
@@ -1,6 +1,19 @@
 from jira.client import JIRA
 import logging
-import pyperclip as pc
+import klembord
+
+color_dict = {
+	"Track": "black",
+	"Specify": "black",
+	"Specify Done": "#BDCB4C",
+	"Implement": "#2B9B62",
+	"Implement Done": "#37797B", 
+	"Review": "#FDC030",
+	"Review Done": "#CD5937",
+	"Validate" : "#B6424C",
+	"Validate Done" : "#1E53A3",
+	"Done" : "#A5397A"
+}
 
 def connect_jira(log, jira_server, jira_user, api_token):
     try:
@@ -13,7 +26,7 @@ def connect_jira(log, jira_server, jira_user, api_token):
         return None
 
 def write_task(i):
-	return i.key + ' ' + i.fields.summary + ' : ' + i.fields.status.name + '\n'
+	return '<b>' + i.key + ' </b> ' + i.fields.summary + ' : <span style="color: ' + color_dict[i.fields.status.name] + '">' + ("<u><b><i>" + i.fields.status.name + "</i></b></u>" if i.fields.status.name == "Done" else i.fields.status.name) + '</span><br/>\n'
 
 
 ###
@@ -23,7 +36,7 @@ assignee_code = ""
 exclude_track = True
 ###
 
-standup = ""
+standup = "<html>"
 log = logging.getLogger(__name__)
 jira = connect_jira(log, "https://fnba.atlassian.net/", email, api_token)
 
@@ -32,12 +45,19 @@ issues_in_proj = jira.search_issues("assignee = " + assignee_code + " AND status
 if(exclude_track):
 	for i in issues_in_proj:
 		if(i.fields.status.name != "Track"):
-			standup += write_task(i)
+			standup+= write_task(i)
 else:
 	for i in issues_in_proj:
-		standup += write_task(i)
+		standup+= write_task(i)
+
+issues_in_proj = jira.search_issues("project = SD AND assignee = " + assignee_code + " AND updateddate >= -2d AND status = DONE")
+
+for i in issues_in_proj:
+	if(i.fields.status.name != "Track"):
+		standup+= write_task(i)
 
 standup = standup[0:len(standup) -1]
-print(standup)
-pc.copy(standup)
-print("Your standup has been copied to your clipboard")
+standup+= "</html>"
+klembord.set_with_rich_text('html', standup)
+print("Your standup has been copied to your clipboard.")
+
